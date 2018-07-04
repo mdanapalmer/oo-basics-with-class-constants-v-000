21c97ad5e7202cd643201a40326dcf7076901635
Dana Palmer <roiai4@hotmail.com>
2018-07-04 03:32:35 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,7 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
+  attr_reader :genre
   
   GENRES = ["Thriller", "Science Fiction", "Romance"]
 
