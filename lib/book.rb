ab20be717e57199ca9ac72e9fbb774affaaccf64
Dana Palmer <roiai4@hotmail.com>
2018-07-06 05:33:22 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,12 +3,11 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
   
+  GENRES = []
+
+  
   def initialize(title)
     @title = title
-  end
-  
-  def title
-    @title
   end
   
   def title=(new_title)
