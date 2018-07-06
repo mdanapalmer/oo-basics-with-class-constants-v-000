49875aad01295075859853dd494c235e656b4001
Dana Palmer <roiai4@hotmail.com>
2018-07-06 05:53:05 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -12,7 +12,7 @@ class Shoe
   def brand=(unique_brand)
     brand = unique_brand
     @brand = brand
-    if BRANDS.include?(brand)
+    if BRANDS == (brand)
       false 
     else BRANDS << brand #if brand isn't already there#
     end
